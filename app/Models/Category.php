855d503f19876471b39d569d90c734710c504947
EchoZahar <zahar.yamal@yandex.ru>
2021-09-12 03:22:50 +0400

    version 1.2.8

---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author');
+
+    }
+
+    public function editor()
+    {
+        return $this->belongsTo(User::class, 'editor');
+    }
+}
---
+++ database/migrations/2021_09_11_213504_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('short_description')->nullable();
+            $table->longText('content');
+            $table->boolean('published')->default(0);
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->unsignedBigInteger('author');
+            $table->unsignedBigInteger('editor')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
